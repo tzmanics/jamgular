3b2b744650bbc63f8d10a4181b77f070d0242ecc
Tara Z. Manicsic <tzmanics@gmail.com>
2019-12-17 12:11:26 -0500

    uses json plugin for userId information

--- scully.config.js
+++ scully.config.js
@@ -1,5 +1,12 @@
 exports.config = {
   projectRoot: "./src/app",
   routes: {
+    "/user/:userId": {
+      type: "json",
+      userId: {
+        url: "https://jsonplaceholder.typicode.com/users",
+        property: "id"
+      }
+    }
   }
 };
