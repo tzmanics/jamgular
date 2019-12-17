ba77765cc152b845ffba3b682e72f12c0ce9d31c
Tara Z. Manicsic <tzmanics@gmail.com>
2019-12-17 09:11:17 -0500

    adds modules and components plus routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "about",
+    loadChildren: () => import("./about/about.module").then(m => m.AboutModule)
+  },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
